5ee6bfaf9940fea43b4f32339b3c37a111a62962
Blackpossum <yosaphatharwindra@gmail.com>
2024-03-09 02:25:11 +0700

    insert using models

--- index.py
+++ index.py
@@ -18,12 +18,19 @@ print('server is comenching')
 @app.route("/")
 def Running_server():
     # insert using SQL 
+    # Session = sessionmaker(connection)
+    # with Session() as s:
+        # execute sql statement 
+        # s.execute(text(" INSERT INTO product (name, price, description, created_at) VALUES ('blood orchid', 250000, 'from borneo rain forrest', '2024-02-07 10:00:00')"))
+        # s.commit()
+
+
+    # insert using Models/SQLAlchemy
+    Newproduct= product(name='Dandelion', price=15000, description='Dandelion Stack miniature,home industry', created_at='2024-02-07 10:00:00')
     Session = sessionmaker(connection)
     with Session() as s:
-        # execute sql statement 
-        s.execute(text(" INSERT INTO product (name, price, description, created_at) VALUES ('blood orchid', 250000, 'from borneo rain forrest', '2024-02-07 10:00:00')"))
+        s.add(Newproduct)
         s.commit()
 
 
-    # insert using Models/SQLAlchemy 
     return 'server running at port 5000'
