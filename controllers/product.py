0902135a4db84bf80d3ac68a84f39b1d00ba4580
Blackpossum <yosaphatharwindra@gmail.com>
2024-03-13 23:03:43 +0700

    read and delete

--- controllers/product.py
+++ controllers/product.py
@@ -5,6 +5,7 @@ from sqlalchemy import select,func
 
 product_routes = Blueprint('product_routes',__name__)
 
+#  show list of product 
 @product_routes.route("/product", methods=['GET'])
 def product_home():
     response_data =dict()
@@ -14,6 +15,12 @@ def product_home():
 
     try :
         product_query = select(Product)
+
+        # add filters for search box query 
+        if request.args.get('query') != None:
+            search_query = request.args.get('query')
+            product_query = product_query.where(Product.name.like(f'%{search_query}%'))
+
         product = session.execute(product_query)
         product = product.scalars()
         response_data['Products'] = product
@@ -25,6 +32,7 @@ def product_home():
 
     return render_template("products/product_home.html",response_data=response_data)
 
+# added ne product in database
 @product_routes.route("/product", methods=['POST'])
 def product_insert():
     new_product = Product(
@@ -46,3 +54,19 @@ def product_insert():
         return {"message" : "failed to created new_product due to error"}
     # operation succes
     return {"message": "new product succesfully created"}
+
+# delete a product in database 
+@product_routes.route("/product/<id>",methods=['DELETE'])
+def product_delete(id):
+    session = Session()
+    session.begin()
+    try:
+        product_to_delete = session.query(Product).filter(Product.id==id).first()
+        session.delete(product_to_delete)
+        session.commit()
+    except Exception as e:
+        session.rollback()
+        print(e)
+        return {"message" : "failed to delete product, please check console"}
+    
+    return {"message" : "product sucessfuly deleted"}
